b16f3e5d4c05dc5ccf619709d9b1b3653d899cc0
Frank Muller <frank@zonarix.com>
2022-07-19 19:24:52 -0500

    g

--- src/test/java/com/furazin/projecttestgithub/ArithmeticTest.java
+++ src/test/java/com/furazin/projecttestgithub/ArithmeticTest.java
@@ -29,4 +29,10 @@ public class ArithmeticTest {
         Arithmetic arith = new Arithmetic(4,2);
         Assert.assertEquals(arith.div(),2);
     }
+
+    @Test
+    public void divTest2() {
+        Arithmetic arith = new Arithmetic(4,2);
+        Assert.assertEquals(arith.div(),3);
+    }
 }
